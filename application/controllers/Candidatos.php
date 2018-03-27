20abde33af92ddc3db1ce1276b7e09e8c936a4dd
luciano.cassettai <luciano.cassettai@gmail.com>
2018-03-27 15:18:09 -0300

    Se agrego json de test para la votacion;

--- application/controllers/Candidatos.php
+++ application/controllers/Candidatos.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 defined('BASEPATH') OR exit('No script allowed');
 
 class Candidatos extends CI_Controller{
-	
+
 	function index(){
 		$this->load->view('templates/header');
 		$this->load->view('candidatos/index');
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -10,43 +10,47 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
 <script>
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-        labels: ["Rectorado", "Economicas", "Tu mina", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# Cantidad de votos',
-            data: [20, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                '#ffabbd',
-                '#7dc1f0',
-                '#fff5dd',
-                '#dbf2f2',
-                '#ebe0ff',
-                '#ffecd9'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
+var jsonfile = {
+   "jsonarray": [{
+      "name": "Joe",
+      "age": 12
+   }, {
+      "name": "Tom",
+      "age": 14
+   }, {
+      "name": "Ron",
+      "age": 14
+   }, {
+      "name": "Jacqueline",
+      "age": 20
+   }, {
+      "name": "Luciano",
+      "age": 30
+   }]
+};
+
+var labels = jsonfile.jsonarray.map(function(e) {
+   return e.name;
 });
+
+var data = jsonfile.jsonarray.map(function(e) {
+   return e.age;
+});;
+
+var ctx = document.getElementById("myChart").getContext('2d');
+var config = {
+   type: 'line',
+   data: {
+      labels: labels,
+      datasets: [{
+         label: 'Graph Line',
+         data: data,
+         backgroundColor: 'rgba(0, 119, 204, 0.3)'
+      }]
+   }
+};
+
+var chart = new Chart(ctx, config);
 </script>
 <script>
 var ctx = document.getElementById("myChart2").getContext('2d');
