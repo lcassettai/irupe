3ec4d3c94e013c83ed43dabe6694ea5490f19524
luciano.cassettai <luciano.cassettai@gmail.com>
2018-03-27 18:55:54 -0300

    Prueba de candidatos

--- application/views/candidatos/index.php
+++ application/views/candidatos/index.php
@@ -2,4 +2,48 @@
 
 <main>
 	<h2 class='text-center'>Candidatos</h2>
+	<div class="row justify-content-md-center">
+		<div class="col-sm-3">
+ 		 <div class="panel text-center">
+ 			 <img src="<?php echo assets_url();?>/img/persona-nula.jpg" alt="" class="img-fluid img-round">
+ 			 <p><b>Luciano Cassettai</b></p>
+ 			 <small>Tecnico Superior en Programacion</small>
+ 		 </div>
+ 	 </div>
+	 <div class="col-sm-3">
+		 <div class="panel text-center">
+			 <img src="<?php echo assets_url();?>/img/persona-nula.jpg" alt="" class="img-fluid img-round">
+			 <p><b>Luciano Cassettai</b></p>
+			 <small>Tecnico Superior en Programacion</small>
+		 </div>
+	 </div>
+	 <div class="col-sm-3">
+		 <div class="panel text-center">
+			 <img src="<?php echo assets_url();?>/img/persona-nula.jpg" alt="" class="img-fluid img-round">
+			 <p><b>Luciano Cassettai</b></p>
+			 <small>Tecnico Superior en Programacion</small>
+		 </div>
+	 </div>
+	 <div class="col-sm-3">
+		 <div class="panel text-center">
+			 <img src="<?php echo assets_url();?>/img/persona-nula.jpg" alt="" class="img-fluid img-round">
+			 <p><b>Luciano Cassettai</b></p>
+			 <small>Tecnico Superior en Programacion</small>
+		 </div>
+	 </div>
+	 <div class="col-sm-3">
+		 <div class="panel text-center">
+			 <img src="<?php echo assets_url();?>/img/persona-nula.jpg" alt="" class="img-fluid img-round">
+			 <p><b>Luciano Cassettai</b></p>
+			 <small>Tecnico Superior en Programacion</small>
+		 </div>
+	 </div>
+	 <div class="col-sm-3">
+		<div class="panel text-center">
+			<img src="<?php echo assets_url();?>/img/persona-nula.jpg" alt="" class="img-fluid img-round">
+			<p><b>Luciano Cassettai</b></p>
+			<small>Tecnico Superior en Programacion</small>
+		</div>
+	</div>
+	</div>
 </main>
--- application/views/votacion/index.php
+++ application/views/votacion/index.php
@@ -4,13 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <main>
 	<div class="container">
-	<h1 class='text-center'>Votacion</h1>
+	<h2 class='text-center'>Votacion</h2>
 	<div class="row">
 		<div class="col-md-6">
 			<h4 class='text-center'>Torta</h4>
 			<div class="grafico">
 				<canvas id="myChart" width="400" height="400"></canvas>
-			</div>			
+			</div>
 		</div>
 		<div class="col-md-6">
 			<h4 class='text-center'>doughnut</h4>
@@ -34,5 +34,3 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </div>
 
 </main>
-
-
